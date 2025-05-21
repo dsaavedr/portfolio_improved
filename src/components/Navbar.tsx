a93160afd09f80ac47d586123d8eda3af090824b
Daniel Saavedra <danielsaavedram@hotmail.com>
2025-05-21 00:39:25 -0500

    Feature/navbar (#2)
    * Deleted placeholder assets and added logo
    
    * Added prettier TW plugin
    
    * Added navigation menu to navbar
    
    * Removed navbar background and added href passthrough for nested links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,16 @@ import { cn } from "@/lib/utils";
 import Link from "next/link";
 import React from "react";
 import DarkModeToggle from "./DarkModeToggle";
+import Logo from "./Logo";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "./ui/navigation-menu";
 
 type Props = {
   className?: string;
@@ -10,13 +20,61 @@ type Props = {
 const Navbar = ({ className }: Props) => {
   return (
     <header className={cn("sticky top-0 z-50 px-4 pb-4", className)}>
-      <div className='fade-bottom bg-background/15 absolute left-0 h-24 w-full backdrop-blur-lg'></div>
-      <div className='max-w-container relative mx-auto'>
-        <nav data-slot='navbar' className='flex items-center, justify-between py-4'>
-          <nav data-slot='navbar-left' className='flex items-center justify-start, gap-4'>
-            <Link href='/'>Logo</Link>
+      <div className="mx-auto max-w-7xl">
+        <nav
+          data-slot="navbar"
+          className="flex items-center justify-between py-4"
+        >
+          <nav
+            data-slot="navbar-left"
+            className="flex items-center justify-start gap-4"
+          >
+            <Link href="/">
+              <Logo />
+            </Link>
+            <NavigationMenu className="ml-10">
+              <NavigationMenuList className="">
+                <NavigationMenuItem>
+                  <Link href="" passHref>
+                    <NavigationMenuLink
+                      className={navigationMenuTriggerStyle()}
+                    >
+                      About me
+                    </NavigationMenuLink>
+                  </Link>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                  <NavigationMenuTrigger>My projects</NavigationMenuTrigger>
+                  <NavigationMenuContent className="p-6 md:w-[400px] lg:w-[500px]">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Expedita sint nam aspernatur placeat corrupti. Dicta totam
+                    deserunt corporis doloremque ab.
+                  </NavigationMenuContent>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                  <Link href="" passHref>
+                    <NavigationMenuLink
+                      className={navigationMenuTriggerStyle()}
+                    >
+                      Contact me
+                    </NavigationMenuLink>
+                  </Link>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                  <NavigationMenuTrigger>Other dropdown</NavigationMenuTrigger>
+                  <NavigationMenuContent className="p-6 md:w-[400px] lg:w-[500px]">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Expedita sint nam aspernatur placeat corrupti. Dicta totam
+                    deserunt corporis doloremque ab.
+                  </NavigationMenuContent>
+                </NavigationMenuItem>
+              </NavigationMenuList>
+            </NavigationMenu>
           </nav>
-          <nav data-slot='navbar-right' className='flex items-center justify-end, gap-4'>
+          <nav
+            data-slot="navbar-right"
+            className="justify-end, flex items-center gap-4"
+          >
             <DarkModeToggle />
           </nav>
         </nav>
