6ca5b0028f91555eb5ef6da1687dc6a882dfef92
Daniel Saavedra <danielsaavedram@hotmail.com>
2025-05-29 16:29:09 -0500

    Feature/admin login (#7)
    * Added Form components and validation packages for form control
    
    * Added login page
    
    * Created login form component and added validation

---
+++ src/app/admin/login/page.tsx
@@ -0,0 +1,11 @@
+import LoginForm from "@/components/LoginForm/LoginForm";
+
+const page = () => {
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <LoginForm />
+    </div>
+  );
+};
+
+export default page;
